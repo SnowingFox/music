b094ef6d2c6d60c63275d0909844aceb068f44a5
snowingfox <1503401882@qq.com>
2022-03-19 17:07:47 +0800

    :zap: feat: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
 
     // quasar({
     //   sassVariables: 'src/assets/scss/ui-vars.scss;'
     // })
   ],
-  define: {
-    'process.env': {
-      mode: 'dev'
-    }
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-  }
+  },
+  server: {
+    https: true,
+    open: '/src/index.html',
+  },
 })
